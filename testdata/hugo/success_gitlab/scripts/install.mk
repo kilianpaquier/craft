e77dd8c3a84c85a22f1559aebce0ed5fd66d47f1
kilianpaquier <kilian@kilianpaquier.com>
2024-08-11 11:03:13 +0000

    chore(makefile): print installing or current version on install scripts
    Signed-off-by: kilianpaquier <kilian@kilianpaquier.com>

--- testdata/hugo/success_gitlab/scripts/install.mk
+++ testdata/hugo/success_gitlab/scripts/install.mk
@@ -4,11 +4,11 @@ define install_hugo
 current_version=$(hugo version || echo "hugo v0.0.0")
 new_version=$(curl -fsSL "https://api.github.com/repos/gohugoio/hugo/releases/latest" | jq -r '.tag_name')
 if echo "${current_version}" | grep -Eq "${new_version}"; then
-	echo "latest hugo version already installed"
+	echo "latest hugo version ${new_version} already installed"
 	exit 0
 fi
 
-echo "installing latest hugo version"
+echo "installing latest hugo version ${new_version}"
 rm -rf "${HOME}/.local/hugo" && mkdir -p "${HOME}/.local/hugo"
 curl -fsSL "https://github.com/gohugoio/hugo/releases/download/${new_version}/hugo_extended_${new_version#v*}_linux-amd64.tar.gz" | (cd "${HOME}/.local/hugo" && tar -xz)
 chmod +x "${HOME}/.local/hugo/hugo" && ln -sf "${HOME}/.local/hugo/hugo" "${HOME}/.local/bin/hugo"
