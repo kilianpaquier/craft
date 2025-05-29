52da516e276f481ec24ad703d8adfa4fb0bd601f
kilianpaquier <git@kilianpaquier.com>
2025-05-29 16:36:43 +0000

    fix(helm): remove default storage requests and limits
    Signed-off-by: kilianpaquier <git@kilianpaquier.com>

---
+++ internal/cobra/main_test.go
@@ -0,0 +1,19 @@
+package cobra_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/charmbracelet/log"
+
+	"github.com/kilianpaquier/craft/pkg/engine"
+)
+
+func TestMain(m *testing.M) {
+	engine.SetLogger(log.NewWithOptions(os.Stderr, log.Options{
+		CallerFormatter: log.ShortCallerFormatter,
+		Level:           log.WarnLevel,
+		ReportCaller:    true,
+	}))
+	os.Exit(m.Run())
+}
