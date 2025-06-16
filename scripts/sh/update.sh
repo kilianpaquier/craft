cb242541f2dbbc523a806dd78a17e9f98bc15ea6
kilianpaquier <git@kilianpaquier.com>
2025-06-16 22:37:19 +0200

    chore(deps): upgrade stefanzweifel/git-auto-commit-action to v6
    Signed-off-by: kilianpaquier <git@kilianpaquier.com>

--- scripts/sh/update.sh
+++ scripts/sh/update.sh
@@ -17,9 +17,13 @@ if [ "$cmd" = "" ]; then
 fi
 log_info "Found craft generator named '$cmd'"
 
-dirs=$(find "$HOME/workspaces" -name testdata -prune -o -name .craft -exec dirname {} +;)
-for dir in $dirs; do
-  log_info "Updating layout of $dir"
-  $cmd -d "$dir"
+workspaces=$(find / -name workspaces 2>&1 | grep -v "Permission denied")
+for workspace in $workspaces; do
+  dirs=$(find "$workspace" -name testdata -prune -o -name .craft -exec dirname {} +;)
+  for dir in $dirs; do
+    log_info "Updating layout of $dir"
+    $cmd -d "$dir"
+  done
+  unset dirs dir
 done
-unset dir
+unset workspaces workspace
